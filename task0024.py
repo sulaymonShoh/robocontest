b3f9b78d0e5456e4b675970f0f71664b3944927d
sulaymonShoh <86634428+sulaymonShoh@users.noreply.github.com>
2022-03-09 12:32:52 +0500

    24 yangilandi

--- task0024.py
+++ task0024.py
@@ -1,18 +1,16 @@
-with open('input.txt', 'r') as fin:
-	a, b =  map(str, fin.read().split('\n'))
-	a, b = a.split(' '), b.split(' ')
-	a, b = [int(x) for x in a], [int(x) for x in b]
+a, b =  map(str, input().split('\n'))
+a, b = a.split(' '), b.split(' ')
+a, b = [int(x) for x in a], [int(x) for x in b]
 
-	if max(a[0], b[0]) == a[0]:
-		a, b = b, a
+if max(a[0], b[0]) == a[0]:
+	a, b = b, a
 
-	rh = b[0]-a[0]
-	rm = b[1]-a[1]
-	if rm<0: rm = 60+rm
+rh = b[0]-a[0]
+rm = b[1]-a[1]
+if rm<0: rm = 60+rm
 
-	rs = b[2]-a[2]
-	if rs<0: rs = 60+rs
+rs = b[2]-a[2]
+if rs<0: rs = 60+rs
 
-	wt = rh*3600 + rm*60 + rs
-	with open('output.txt', 'w') as fout:
-		fout.write(str(wt))
+wt = rh*3600 + rm*60 + rs
+print(wt)
