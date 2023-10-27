d1bb74e79a80939f41d1dfab0784a53dd4b4bf5f
Sulaymonqori Abdumutallibov <86634428+sulaymonShoh@users.noreply.github.com>
2023-10-27 20:08:45 +0500

    27/10/23 update

--- task0009.py
+++ task0009.py
@@ -1,6 +1,6 @@
-cn = int(input())
-nums = list(map(int, input().split(' ')))
-for i in nums:
-	if nums.count(i)==1:
-		print(i)
-		break
+# cn = int(input())
+# nums = list(map(int, input().split(' ')))
+# for i in nums:
+# 	if nums.count(i)==1:
+# 		print(i)
+# 		break
